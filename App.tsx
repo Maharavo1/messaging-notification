4185246f0fe39237885c6e86c8c27a31d42af97e
Rado23-04 <hei.rado.2@gmail.com>
2025-02-10 13:41:37 +0300

    feat:Request notification permissions and initialize FCM token

--- App.tsx
+++ App.tsx
@@ -84,3 +84,31 @@ const linking = {
     };
   },
 }
+function App(): React.JSX.Element {
+  useEffect(()=>{
+   const requestUserPermission = async () => {
+     PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+      const token = await messaging().getToken();
+      console.log('FCM token:', token);
+    }
+  };
+
+  requestUserPermission();
+  },[])
+
+ return (
+   <NavigationContainer linking={linking} fallback={<ActivityIndicator animating />}>
+   <Stack.Navigator initialRouteName='Home'>
+     <Stack.Screen name="Home" component={Home} />
+     <Stack.Screen name="Settings" component={Setting} />
+   </Stack.Navigator>
+ </NavigationContainer>
+ );
+}
